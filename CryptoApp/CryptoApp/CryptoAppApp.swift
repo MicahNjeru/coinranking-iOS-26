2c8bd45a0ade8dab99f6c67d74f538cf814225ea
Micah Njeru <micahnjeru1@gmail.com>
2025-11-30 17:53:13 +0300

    First commit

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Micah Njeru on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
